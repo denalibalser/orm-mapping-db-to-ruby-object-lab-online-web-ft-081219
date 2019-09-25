4c80dc857a68d9f271c74265004d909911938a4e
Denali Balser <denali.balser167@gmail.com>
2019-09-25 20:48:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -53,14 +53,9 @@ class Student
     end
   end
 
-  def self.first_X_students_in_grade_10(x)
-    sql = <<-SQL
-      SELECT *
-      FROM students
-      WHERE students.grade = 10
-      LIMIT ?
-      SQL
-      DB[:conn].execute(sql, x)
+  def self.first_X_students_in_grade_10(num)
+    sql = "SELECT * FROM students WHERE grade=10 ORDER BY students.id LIMIT ?;"
+    DB[:conn].execute(sql, num)
   end
 
   def save
