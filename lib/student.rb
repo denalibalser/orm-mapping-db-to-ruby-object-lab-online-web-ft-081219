3d1ca7a5dedbc8f30793ecf58852a3a851aea8ec
Denali Balser <denali.balser167@gmail.com>
2019-09-25 21:19:40 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -82,7 +82,7 @@ class Student
       SELECT *
       FROM students
       WHERE students.grade = #{x}
-      LIMIT 1;
+      LIMIT 1
       SQL
       DB[:conn].execute(sql).map do |row|
           self.new_from_db(row)
